42b6c13cfe6bd7109d52b4d410bd5da247e1e9e0
sean <1026025056@qq.com>
2023-12-22 15:40:11 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,22 @@
 #include "ethernet_api.h"
 #include <esp_heap_caps.h>
 
+#include <M5UnitOLED.h>
+#include <WiFi.h>
+
 #define SERVER_PORT 80
+
+// for factorytest
+M5UnitOLED display;
+M5Canvas canvas(&display);
+
+void factory_oled_init();
+void factory_wifi_test();
+
+bool factory_test_mode = false;
+
+// for poe cam server
+static void jpegStream(EthernetClient* client);
 
 static EthernetServer server(SERVER_PORT);
 
@@ -18,8 +33,29 @@ void setup() {
 
     Serial.printf("Camera init.....\r\n");
 
+    delay(100);
+
+    if (!digitalRead(37)) {
+        delay(500);
+        if (!digitalRead(37)) {
+            factory_test_mode = true;
+        }
+    }
+
+    if (factory_test_mode) {
+        factory_oled_init();
+        factory_wifi_test();
+    }
+
     // Default Use FRAMESIZE_XGA 1024x768
     while (camrea_init() != ESP_OK) {
+        if (factory_test_mode) {
+            canvas.clear();
+            canvas.setCursor(0, 0);
+            canvas.printf("Camera init failed\r\n");
+            canvas.pushSprite(0, 0);
+        }
+
         Serial.printf("Camera init failed\r\n");
         digitalWrite(0, 1);
         delay(1000);
@@ -28,7 +64,21 @@ void setup() {
     }
     Serial.printf("Cam Init Success\r\n");
 
+    if (factory_test_mode) {
+        canvas.clear();
+        canvas.setCursor(0, 0);
+        canvas.printf("Camera init Success\r\n");
+        canvas.pushSprite(0, 0);
+    }
+
     while (!eth_init()) {
+        if (factory_test_mode) {
+            canvas.clear();
+            canvas.setCursor(0, 0);
+            canvas.printf("Eth init failed\r\n");
+            canvas.pushSprite(0, 0);
+        }
+
         Serial.printf("Eth init failed\r\n");
         digitalWrite(0, 1);
         delay(1000);
@@ -42,6 +92,34 @@ void setup() {
     Serial.printf("Server Init Success\r\n");
     Serial.print("Listen on: ");
     Serial.println(eth_get_ip() + ":" + String(SERVER_PORT));
+
+    if (factory_test_mode) {
+        canvas.clear();
+        canvas.setCursor(0, 0);
+        canvas.println("Open " + eth_get_ip());
+        canvas.pushSprite(0, 0);
+    }
+}
+
+void loop() {
+    // listen for incoming clients
+    EthernetClient client = server.available();
+    if (client) {
+        Serial.println("new client from: " + client.remoteIP().toString());
+        // an http request ends with a blank line
+        boolean currentLineIsBlank = true;
+
+        while (client.connected()) {
+            if (client.available()) {
+                jpegStream(&client);
+            }
+        }
+        // give the web browser time to receive the data
+        delay(1);
+        // close the connection:
+        client.stop();
+        Serial.println("client disconnected");
+    }
 }
 
 // used to image stream
@@ -98,6 +176,11 @@ static void jpegStream(EthernetClient* client) {
 
         esp_camera_fb_return(fb);
         fb = NULL;
+        if (digitalRead(37)) {
+            digitalWrite(0, 1);
+        } else {
+            digitalWrite(0, 0);
+        }
     }
 
 client_exit:
@@ -108,23 +191,52 @@ client_exit:
     Serial.printf("Image stream end\r\n");
 }
 
-void loop() {
-    // listen for incoming clients
-    EthernetClient client = server.available();
-    if (client) {
-        Serial.println("new client from: " + client.remoteIP().toString());
-        // an http request ends with a blank line
-        boolean currentLineIsBlank = true;
+void factory_oled_init() {
+    display.init(Ext_PIN_2, Ext_PIN_1);
+    display.setRotation(3);
+    canvas.setColorDepth(1);  // mono color
+    canvas.setFont(&fonts::efontCN_12);
+    canvas.setTextWrap(true);
+    canvas.setTextSize(1);
+    canvas.createSprite(display.width(), display.height());
+}
 
-        while (client.connected()) {
-            if (client.available()) {
-                jpegStream(&client);
+void factory_wifi_test() {
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0) {
+        canvas.fillSprite(0x0);
+        canvas.setCursor(0, 0);
+        canvas.printf("no wifi found\r\n");
+        canvas.pushSprite(0, 0);
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < (n > 5 ? 5 : n); ++i) {
+            // Print SSID and RSSI for each network found
+            if (canvas.getCursorY() > 40) {
+                canvas.fillSprite(0x0);
+                canvas.setCursor(0, 0);
+                canvas.pushSprite(0, 0);
+
+            } else {
+                canvas.print(i + 1);
+                canvas.print(": ");
+                canvas.print(WiFi.SSID(i));
+                canvas.print(" (");
+                canvas.print(WiFi.RSSI(i));
+                canvas.print(")");
+                canvas.println(
+                    (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+                canvas.pushSprite(0, 0);
             }
+            delay(500);
         }
-        // give the web browser time to receive the data
-        delay(1);
-        // close the connection:
-        client.stop();
-        Serial.println("client disconnected");
     }
+
+    canvas.fillSprite(0x0);
+    canvas.setCursor(0, 0);
+    canvas.printf("WiFi Scan: %d\r\n", n);
+    canvas.pushSprite(0, 0);
+    delay(2000);
 }
